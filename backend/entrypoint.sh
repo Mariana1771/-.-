094dfb3324d84f81861a90688cf34ea59a81e3b2
mariana <mariankaonisko17@gmail.com>
2026-05-12 01:14:42 +0300

    Add createsuperuser to entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput || true
+
 
 exec gunicorn lingua.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 60
 
